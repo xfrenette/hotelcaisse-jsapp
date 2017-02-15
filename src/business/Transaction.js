ae167dc73a135c8dd115b723f8426a2c96d2a05c
Xavier Frenette <xavier@xavierfrenette.com>
2017-02-14 19:43:23 -0500

    Transaction: now serializable

--- src/business/Transaction.js
+++ src/business/Transaction.js
@@ -1,3 +1,7 @@
+import { serializable, date, object } from 'serializr';
+import TransactionMode from './TransactionMode';
+import { decimal } from '../vendor/serializr/propSchemas';
+
 /**
  * A Transaction is an exchange of money between the business
  * and the customer. The Transaction is generally added
@@ -13,27 +17,36 @@ class Transaction {
 	 *
 	 * @type {Decimal}
 	 */
+	@serializable(decimal())
 	amount = null;
 	/**
 	 * Transaction mode used.
 	 *
 	 * @type {TransactionMode}
 	 */
+	@serializable(object(TransactionMode))
 	transactionMode = null;
 	/**
 	 * Optionnal note.
 	 *
 	 * @type {String}
 	 */
+	@serializable
 	note = '';
 	/**
 	 * Creation date time.
 	 *
 	 * @type {Date}
 	 */
+	@serializable(date())
 	createdAt = null;
 	/**
-	 * Register where this transaction is saved. It is set when calling Register#addTransaction(Transaction).
+	 * Register where this transaction is saved. It is set when calling
+	 * Register#addTransaction(Transaction).
+	 *
+	 * Note that we need this information only when creating new Transaction, not when viewing
+	 * already existing ones, so this reference will probably stay null if this is not a new
+	 * Transaction. For this reason, the register property is not serialized.
 	 *
 	 * @type {Register}
 	 */
--- tests/business/Transaction.test.js
+++ tests/business/Transaction.test.js
@@ -1,6 +1,7 @@
 import Transaction from 'business/Transaction';
 import TransactionMode from 'business/TransactionMode';
 import Decimal from 'decimal.js';
+import { serialize, deserialize } from 'serializr';
 
 let transaction;
 
@@ -26,5 +27,62 @@ describe('constructor()', () => {
 		const transactionMode = new TransactionMode(10);
 		transaction = new Transaction(new Decimal(1), transactionMode);
 		expect(transaction.transactionMode).toBe(transactionMode);
+	});
+});
+
+describe('serializing', () => {
+	let data;
+
+	beforeEach(() => {
+		transaction.note = 'test-note';
+		data = serialize(transaction);
+	});
+
+	test('saves transactionMode object', () => {
+		const tmData = serialize(transaction.transactionMode);
+		expect(data.transactionMode).toEqual(tmData);
+	});
+
+	test('saves primitives', () => {
+		expect(data).toEqual(expect.objectContaining({
+			note: transaction.note,
+			createdAt: expect.any(Number),
+		}));
+	});
+
+	test('saves amount', () => {
+		expect(typeof data.amount).toBe('string');
+	});
+});
+
+describe('deserializing', () => {
+	let newTransaction;
+	const jsonObject = {
+		amount: '-1.23',
+		transactionMode: {
+			name: 'test-tm',
+		},
+		note: 'test-note',
+		createdAt: (new Date()).getTime(),
+	};
+
+	beforeEach(() => {
+		newTransaction = deserialize(Transaction, jsonObject);
+	});
+
+	test('restores createdAt', () => {
+		expect(newTransaction.createdAt).toBeInstanceOf(Date);
+	});
+
+	test('restores primitives', () => {
+		expect(newTransaction.note).toBe(jsonObject.note);
+	});
+
+	test('restores transactionMode', () => {
+		expect(newTransaction.transactionMode).toBeInstanceOf(TransactionMode);
+	});
+
+	test('restores amount', () => {
+		expect(newTransaction.amount).toBeInstanceOf(Decimal);
 	});
 });
