b964afa03eb155764203aec2335ff735bf15c44e
Xavier Frenette <xavier@xavierfrenette.com>
2017-02-20 12:28:35 -0500

    plugins/Plugin: comments cleanup

--- src/plugins/Plugin.js
+++ src/plugins/Plugin.js
@@ -1,30 +1,23 @@
-// TODO
 /**
- * A Plugin is any class that will live with the application.
- * Note: a plugin should wait the bootstrap call before doing
- * anything, since multiple instances are not yet created when
- * the constructor is called.
+ * A Plugin is any class that will live with the application. Note: a plugin should wait the
+ * bootstrap call before doing anything, since multiple instances are not yet created when the
+ * constructor is called.
  */
 class Plugin {
-	application = null;
-
 	/**
-	 * Bootstrapping where the plugin may initialize data, but
-	 * should not modify anything until start() is called.
-	 *
-	 * @param {Application} application
+	 * Bootstrapping where the plugin may initialize data, but should not modify anything until
+	 * start() is called.
 	 */
-	bootstrap(application) {
-		// this.application = application
+	bootstrap() {
 	}
 
 	/**
-	 * When start is called, the application is ready. Returns
-	 * a Promise when finished.
+	 * When start is called, the application is ready. Returns a Promise when finished.
 	 *
 	 * @return {Promise}
 	 */
 	start() {
-
 	}
 }
+
+export default Plugin;
