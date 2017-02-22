d494aaadff62394b830bb41f2d8e0e901c2e49b4
Xavier Frenette <xavier@xavierfrenette.com>
2017-02-22 13:17:17 -0500

    Customer: added clone() and isEqualTo() methods

--- src/business/Customer.js
+++ src/business/Customer.js
@@ -4,6 +4,28 @@ import { serializable } from 'serializr';
 class Customer {
 	@serializable
 	name = '';
+
+	/**
+	 * Returns a clone of this Customer (a new object)
+	 *
+	 * @return {Customer}
+	 */
+	clone() {
+		const newCustomer = new Customer();
+		newCustomer.name = this.name;
+
+		return newCustomer;
+	}
+
+	/**
+	 * Returns true if this Customer is equal (same values) as the other Customer.
+	 *
+	 * @param {Customer} other
+	 * @return {Boolean}
+	 */
+	isEqualTo(other) {
+		return this.name === other.name;
+	}
 }
 
 export default Customer;
--- tests/business/Customer.test.js
+++ tests/business/Customer.test.js
@@ -8,6 +8,42 @@ beforeEach(() => {
 	customer.name = 'test-name';
 });
 
+describe('clone()', () => {
+	let clone;
+
+	beforeEach(() => {
+		clone = customer.clone();
+	});
+
+	test('returns different Customer', () => {
+		expect(clone).toBeInstanceOf(Customer);
+		expect(clone).not.toBe(customer);
+	});
+
+	test('has same name', () => {
+		expect(clone.name).toBe(customer.name);
+	});
+});
+
+describe('isEqualTo()', () => {
+	let other;
+
+	beforeEach(() => {
+		other = customer.clone();
+	});
+
+	test('returns true if equal', () => {
+		expect(customer.isEqualTo(other)).toBe(true);
+	});
+
+	describe('returns false if', () => {
+		test('different name', () => {
+			other.name = `${customer.name} (other)`;
+			expect(customer.isEqualTo(other)).toBe(false);
+		});
+	});
+});
+
 describe('serializing', () => {
 	let data;
 
