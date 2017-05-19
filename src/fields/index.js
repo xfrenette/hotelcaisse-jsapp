6c76f8b6a9642c9ddf24eaddc539f4be309bfa5b
Xavier Frenette <xavier@xavierfrenette.com>
2017-05-18 21:24:22 -0400

    fields: added an index

---
+++ src/fields/index.js
@@ -0,0 +1,13 @@
+import Field from './Field';
+import EmailField from './EmailField';
+import TextField from './TextField';
+import NumberField from './NumberField';
+import SelectField from './SelectField';
+
+export default {
+	Field,
+	EmailField,
+	TextField,
+	NumberField,
+	SelectField,
+};
