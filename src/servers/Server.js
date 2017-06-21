7408f98de75e25e83285a303a0735ed4fa3af65f
Xavier Frenette <xavier@xavierfrenette.com>
2017-06-21 17:02:55 -0400

    servers/Server: added the getOrders method

--- src/servers/Server.js
+++ src/servers/Server.js
@@ -1,5 +1,20 @@
+/**
+ * A Server class represents an external entity that can return data for the Application, that can
+ * save data or that can execute actions the Application itself cannot do (ex: send emails). This
+ * instance does nothing and should be implemented.
+ */
 class Server {
-
+	/**
+	 * Returns [quantity] Orders following the [from] Order. It is the Server that determines the order
+	 * of the Orders. Returns an Promise that resolves with an array of Order.
+	 *
+	 * @param {Order} from
+	 * @param {Number} quantity
+	 * @return {Promise}
+	 */
+	getOrders(from, quantity) {
+		return Promise.resolve([]);
+	}
 }
 
 export default Server;
