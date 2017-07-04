5f46281738e102f9d350a44b80a7fe25412d201f
Xavier Frenette <xavier@xavierfrenette.com>
2017-07-04 09:00:15 -0400

    plugins/autosave/business/ToWriter: implemented and tested

---
+++ src/plugins/autosave/business/ToWriter.js
@@ -0,0 +1,52 @@
+import Plugin from '../../Plugin';
+
+/**
+ * Plugin that listens to any change to the Business instance (and its Register) and writes the
+ * *whole* Business instance to a writer; that is for each modification, writes the whole Business
+ * instance.
+ */
+class ToWriter extends Plugin {
+	/**
+	 * Writer where to write the Business instance
+	 *
+	 * @type {Writer}
+	 */
+	writer = null;
+
+	constructor(writer) {
+		super();
+		this.writer = writer;
+	}
+
+	/**
+	 * When starting, start listening to events
+	 */
+	start() {
+		this.registerListeners();
+	}
+
+	/**
+	 * Listen to Business events. For each of them, we simply write the whole Business instance to the
+	 * writer.
+	 */
+	registerListeners() {
+		const business = this.application.business;
+		const callback = () => { this.writeBusiness(); };
+
+		business.on('registerOpen', callback);
+		business.on('registerClose', callback);
+		business.on('cashMovementAdd', callback);
+		business.on('cashMovementRemove', callback);
+		business.on('newOrder', callback);
+		business.on('orderChange', callback);
+	}
+
+	/**
+	 * Writes the whole Business instance to the Writer
+	 */
+	writeBusiness() {
+		this.writer.write(this.application.business);
+	}
+}
+
+export default ToWriter;
---
+++ tests/plugins/autosave/business/ToWriter.test.js
@@ -0,0 +1,65 @@
+import ToWriter from 'plugins/autosave/business/ToWriter';
+import Application from 'Application';
+import Business from 'business/Business';
+import CashMovement from 'business/CashMovement';
+import Order from 'business/Order';
+import Register, { STATES as REGISTER_STATES } from 'business/Register';
+
+let toWriter;
+let writer;
+let application;
+let business;
+let register;
+
+beforeEach(() => {
+	writer = {
+		write: jest.fn(),
+	};
+	toWriter = new ToWriter(writer);
+	application = new Application();
+	business = new Business();
+	register = new Register();
+	business.deviceRegister = register;
+	application.business = business;
+	toWriter.bootstrap(application);
+	toWriter.start();
+});
+
+test('register opens', () => {
+	register.open('test');
+	expect(writer.write).toHaveBeenCalledWith(business);
+});
+
+test('register closes', () => {
+	register.close('test');
+	expect(writer.write).toHaveBeenCalledWith(business);
+});
+
+test('CashMovement added', () => {
+	const cashMovement = new CashMovement();
+	register.addCashMovement(cashMovement);
+	expect(writer.write).toHaveBeenCalledWith(business);
+});
+
+test('CashMovement removed', () => {
+	const cashMovement = new CashMovement();
+	register.addCashMovement(cashMovement);
+	register.removeCashMovement(cashMovement);
+	expect(writer.write).toHaveBeenCalledTimes(2);
+	expect(writer.write).toHaveBeenLastCalledWith(business);
+});
+
+test('new Order', () => {
+	const order = new Order();
+	business.addOrder(order);
+	expect(writer.write).toHaveBeenCalledWith(business);
+});
+
+test('order change', () => {
+	const changes = { a: 'b' };
+	const order = new Order();
+	order.getChanges = jest.fn().mockImplementation(() => changes);
+	business.orders.push(order);
+	order.commitChanges();
+	expect(writer.write).toHaveBeenCalledWith(business);
+});
