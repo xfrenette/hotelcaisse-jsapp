f004354e368a9a2b88dc601db4f0f4fd5626cc7b
Xavier Frenette <xavier@xavierfrenette.com>
2017-07-04 09:35:51 -0400

    Added io/dual/Dual

---
+++ src/io/dual/Dual.js
@@ -0,0 +1,22 @@
+/**
+ * A Dual class is simply a merge of a Reader and a Writer class.
+ */
+class Dual {
+	/**
+	 * @see io/readers/Reader
+	 * @return {Promise}
+	 */
+	read() {
+		return Promise.resolve(null);
+	}
+	/**
+	 * @see io/writers/Writer
+	 * @return {Promise}
+	 */
+	// eslint-disable-next-line no-unused-vars
+	write(data) {
+		return Promise.resolve();
+	}
+}
+
+export default Dual;
