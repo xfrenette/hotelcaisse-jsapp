746cc76292d9e5f98037e9ed0ffb1faf15de092f
Xavier Frenette <xavier@xavierfrenette.com>
2017-09-06 14:00:32 -0400

    ApiServerUpdatesListener: new plugin that listen to data updates from the api Closes #7

---
+++ src/plugins/ApiServerUpdatesListener.js
@@ -0,0 +1,51 @@
+import Plugin from './Plugin';
+
+/**
+ * Plugin that listens to business and register update events from the Api server and updates
+ * the data on the application instance.
+ */
+class ApiServerUpdatesListener extends Plugin {
+	/**
+	 * Api server instance
+	 * @type {Api}
+	 */
+	server = null;
+
+	constructor(server) {
+		super();
+		this.server = server;
+	}
+
+	/**
+	 * On start, setup the listeners
+	 */
+	start() {
+		this.setupListeners();
+	}
+
+	/**
+	 * Listen to `businessUpdate` and `registerUpdate` events
+	 */
+	setupListeners() {
+		this.server.on('businessUpdate', this.updateBusiness.bind(this));
+		this.server.on('registerUpdate', this.updateRegister.bind(this));
+	}
+
+	/**
+	 * Updates the business instance of the application with the received business.
+	 * @param {Business} business
+	 */
+	updateBusiness(business) {
+		this.application.business.update(business);
+	}
+
+	/**
+	 * Updates the register instance of the application with the received register.
+	 * @param {Register} register
+	 */
+	updateRegister(register) {
+		this.application.register.update(register);
+	}
+}
+
+export default ApiServerUpdatesListener;
---
+++ tests/plugins/ApiServerUpdatesListener.test.js
@@ -0,0 +1,47 @@
+import { serialize } from 'serializr';
+import Listener from 'plugins/ApiServerUpdatesListener';
+import Application from 'Application';
+import Business from 'business/Business';
+import Register from 'business/Register';
+import Room from 'business/Room';
+import Server from 'servers/Api';
+
+let listener;
+let application;
+let server;
+
+beforeEach(() => {
+	server = new Server();
+
+	application = new Application();
+	application.business = new Business();
+	application.register = new Register();
+
+	listener = new Listener(server);
+	listener.bootstrap(application);
+	listener.start();
+});
+
+test('business update', (done) => {
+	const newBusiness = new Business();
+	const room = new Room();
+	room.name = 'test room';
+	newBusiness.rooms.push(room);
+
+	application.business.update = (updateData) => {
+		expect(updateData).toEqual(newBusiness);
+		done();
+	};
+	server.processResponseBusiness({ business: serialize(newBusiness) });
+});
+
+test('register update', (done) => {
+	const newRegister = new Register();
+	newRegister.uuid = 'test-register-uuid';
+
+	application.register.update = (updateData) => {
+		expect(updateData).toEqual(newRegister);
+		done();
+	};
+	server.processResponseRegister({ deviceRegister: serialize(newRegister) });
+});
